264eb87054ca0d6d356e584cc436f2f04ad4aef4
GitHub Action <action@github.com>
2021-09-15 10:28:48 +0000

    update

--- core/test.go
+++ core/test.go
@@ -9,23 +9,35 @@ import (
 func init() {
 	AddCommand("", []Function{
 		{
-			Rules: []string{"set ? ?"},
+			Rules: []string{"set ? ? ?"},
 			Handle: func(s im.Sender) interface{} {
-				sillyGirl.Set(s.Get(0), s.Get(1))
+				b := Bucket(s.Get(0))
+				if !IsBucket(b) {
+					return errors.New("不存在的存储桶")
+				}
+				b.Set(s.Get(1), s.Get(2))
 				return "设置成功"
 			},
 		},
 		{
-			Rules: []string{"delete ?"},
+			Rules: []string{"delete ? ?"},
 			Handle: func(s im.Sender) interface{} {
-				sillyGirl.Set(s.Get(), "")
+				b := Bucket(s.Get(0))
+				if !IsBucket(b) {
+					return errors.New("不存在的存储桶")
+				}
+				b.Set(s.Get(1), "")
 				return "删除成功"
 			},
 		},
 		{
-			Rules: []string{"get ?"},
+			Rules: []string{"get ? ?"},
 			Handle: func(s im.Sender) interface{} {
-				v := sillyGirl.Get(s.Get())
+				b := Bucket(s.Get(0))
+				if !IsBucket(b) {
+					return errors.New("不存在的存储桶")
+				}
+				v := b.Get(s.Get(1))
 				if v == "" {
 					return errors.New("空值")
 				}
@@ -33,4 +45,13 @@ func init() {
 			},
 		},
 	})
+}
+
+func IsBucket(b Bucket) bool {
+	for i := range Buckets {
+		if Buckets[i] == b {
+			return true
+		}
+	}
+	return false
 }

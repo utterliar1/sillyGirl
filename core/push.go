5ac09735952257bda80f8b19151c34712661d248
GitHub Action <action@github.com>
2021-09-17 02:36:59 +0000

    update

--- core/push.go
+++ core/push.go
@@ -1,9 +1,25 @@
 package core
 
 var Pushs = map[string]func(int, string){}
+var GroupPushs = map[string]func(int, int, string){}
 
 func Push(class string, uid int, content string) {
 	if push, ok := Pushs[class]; ok {
 		push(uid, content)
+	}
+}
+
+type Chat struct {
+	Class  string
+	ID     int
+	UserID int
+}
+
+func (ct *Chat) Push(content interface{}) {
+	switch content.(type) {
+	case string:
+		if push, ok := GroupPushs[ct.Class]; ok {
+			push(ct.ID, ct.UserID, content.(string))
+		}
 	}
 }

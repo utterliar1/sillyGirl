a37b41fbf6eee23497e42da0774fc731052a5fc9
GitHub Action <action@github.com>
2021-09-26 10:28:10 +0000

    update

--- core/init.go
+++ core/init.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"os"
 	"regexp"
-	"strings"
 	"time"
 )
 
@@ -18,17 +17,23 @@ func init() {
 			Daemon()
 		}
 	}
+	_, err := os.Stat("/etc/sillyGirl/")
+	if err != nil {
+		os.MkdirAll("/etc/sillyGirl/", os.ModePerm)
+	}
 	initStore()
-	initToHandleMessage()
+	ReadYaml(ExecPath+"/conf/", &Config, "https://raw.githubusercontent.com/cdle/sillyGirl/main/conf/demo_config.yaml")
 	InitReplies()
+	initToHandleMessage()
 	file, err := os.Open("/etc/sillyGirl/sets.conf")
 	if err == nil {
 		scanner := bufio.NewScanner(file)
 		for scanner.Scan() {
 			line := scanner.Text()
-			if regexp.MustCompile(`^\s*set`).MatchString(line) {
-				Senders <- &Faker{
-					Message: strings.Trim(line, " "),
+			if v := regexp.MustCompile(`^\s*set\s+(\S+)\s+(\S+)\s+(\S+)`).FindStringSubmatch(line); len(v) > 0 {
+				b := Bucket(v[1])
+				if b.Get(v[2]) != v[3] {
+					b.Set(v[2], v[3])
 				}
 			}
 		}

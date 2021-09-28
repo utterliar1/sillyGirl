b4fd9f54d13d827290d608c9d211e5aef8b2371b
GitHub Action <action@github.com>
2021-09-28 04:34:41 +0000

    update

--- im/qq/sender.go
+++ im/qq/sender.go
@@ -51,6 +51,8 @@ func (sender *Sender) GetChatID() int {
 	switch sender.Message.(type) {
 	case *message.GroupMessage:
 		id = int(sender.Message.(*message.GroupMessage).GroupCode)
+	case *message.TempMessage:
+		id = int(sender.Message.(*message.TempMessage).GroupCode)
 	}
 	return id
 }
@@ -192,11 +194,11 @@ func (sender *Sender) Reply(msgs ...interface{}) (int, error) {
 				sender.Reply(err)
 				return 0, nil
 			} else {
-				bot.SendPrivateMessage(m.Sender.Uin, int64(qq.GetInt("groupCode")), &message.SendingMessage{Elements: []message.IMessageElement{&coolq.LocalImageElement{Stream: bytes.NewReader(data)}}})
+				bot.SendPrivateMessage(m.Sender.Uin, m.GroupCode, &message.SendingMessage{Elements: []message.IMessageElement{&coolq.LocalImageElement{Stream: bytes.NewReader(data)}}})
 			}
 		}
 		if content != "" {
-			bot.SendPrivateMessage(m.Sender.Uin, int64(qq.GetInt("groupCode")), &message.SendingMessage{Elements: []message.IMessageElement{&message.TextElement{Content: content}}})
+			bot.SendPrivateMessage(m.Sender.Uin, m.GroupCode, &message.SendingMessage{Elements: []message.IMessageElement{&message.TextElement{Content: content}}})
 		}
 	case *message.GroupMessage:
 		var id int32

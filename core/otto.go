083d41f517caadbe41ade47b1df7c2eb4c802b3e
GitHub Action <action@github.com>
2021-10-06 02:36:15 +0000

    update

--- core/otto.go
+++ core/otto.go
@@ -122,7 +122,7 @@ func init() {
 			data = string(v)
 		}
 		rules := []string{}
-		for _, res := range regexp.MustCompile(`\[rule:([^\[\]]+)\]`).FindAllStringSubmatch(data, -1) {
+		for _, res := range regexp.MustCompile(`\[rule:(.+)\]`).FindAllStringSubmatch(data, -1) {
 			rules = append(rules, strings.Trim(res[1], " "))
 		}
 		cron := ""
@@ -139,13 +139,17 @@ func init() {
 		}
 		var handler = func(s Sender) interface{} {
 			template := data
-			for k, v := range s.GetMatch() {
-				template = strings.Replace(template, fmt.Sprintf(`param(%d)`, k+1), fmt.Sprintf(`"%s"`, v), -1)
-			}
+
 			template = strings.Replace(template, "ImType()", fmt.Sprintf(`"%s"`, s.GetImType()), -1)
 			template = strings.Replace(template, "GetChatID()", fmt.Sprint(s.GetChatID()), -1)
+			param := func(call otto.Value) otto.Value {
+				i, _ := call.ToInteger()
+				v, _ := otto.ToValue(s.Get(int(i - 1)))
+				return v
+			}
 			vm := otto.New()
 			vm.Set("set", set)
+			vm.Set("param", param)
 			vm.Set("get", get)
 			vm.Set("request", request)
 			vm.Set("push", push)

3e75689218de9536089e33c758ca860461be013f
GitHub Action <action@github.com>
2021-10-13 14:23:25 +0000

    update

--- develop/qinglong/cron.go
+++ develop/qinglong/cron.go
@@ -279,9 +279,11 @@ func GetCronID(s core.Sender, keyword string) (*Cron, error) {
 		}
 		if strings.Contains(cron.Name, keyword) {
 			cs = append(cs, cron)
+			continue
 		}
 		if strings.Contains(cron.Command, keyword) {
 			cs = append(cs, cron)
+			continue
 		}
 		// if regexp.MustCompile(keyword+"$").FindString(cron.Command) != "" {
 		// 	cs = append(cs, cron)
@@ -316,7 +318,7 @@ func GetCronID(s core.Sender, keyword string) (*Cron, error) {
 			}
 		}
 	} else {
-		cron = cs[1]
+		cron = cs[0]
 	}
 	return &cron, nil
 }

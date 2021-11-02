a4b7aa041c0b4dc2c8b8b96c9800d8f1a2381972
GitHub Action <action@github.com>
2021-11-02 01:22:32 +0000

    update

--- core/function.go
+++ core/function.go
@@ -66,7 +66,7 @@ func AddCommand(prefix string, cmds []Function) {
 			}
 			cmds[j].Rules[i] = strings.Replace(cmds[j].Rules[i], "(", `[(]`, -1)
 			cmds[j].Rules[i] = strings.Replace(cmds[j].Rules[i], ")", `[)]`, -1)
-			cmds[j].Rules[i] = regexp.MustCompile(`\?$`).ReplaceAllString(cmds[j].Rules[i], `(.+)`)
+			cmds[j].Rules[i] = regexp.MustCompile(`\?$`).ReplaceAllString(cmds[j].Rules[i], `([\s\S]+)`)
 			cmds[j].Rules[i] = strings.Replace(cmds[j].Rules[i], " ", `\s+`, -1)
 			cmds[j].Rules[i] = strings.Replace(cmds[j].Rules[i], "?", `(\S+)`, -1)
 			cmds[j].Rules[i] = "^" + cmds[j].Rules[i] + "$"
@@ -87,26 +87,6 @@ func AddCommand(prefix string, cmds []Function) {
 
 func handleMessage(sender Sender) {
 	defer sender.Finish()
-	recall := sillyGirl.Get("recall")
-	if recall != "" {
-		recalled := false
-		for _, v := range strings.Split(recall, "&") {
-			reg, err := regexp.Compile(v)
-			if err == nil {
-				if reg.FindString(sender.GetContent()) != "" {
-					if !sender.IsAdmin() && sender.GetImType() != "wx" {
-						sender.Delete()
-						sender.Reply("本妞清除了不好的消息～", time.Duration(time.Second))
-						recalled = true
-						break
-					}
-				}
-			}
-		}
-		if recalled == true {
-			return
-		}
-	}
 
 	defer func() {
 		logs.Info("%v ==> %v", sender.GetContent(), "finished")
@@ -205,6 +185,26 @@ func handleMessage(sender Sender) {
 		return nil
 	})
 
+	recall := sillyGirl.Get("recall")
+	if recall != "" {
+		recalled := false
+		for _, v := range strings.Split(recall, "&") {
+			reg, err := regexp.Compile(v)
+			if err == nil {
+				if reg.FindString(sender.GetContent()) != "" {
+					if !sender.IsAdmin() && sender.GetImType() != "wx" {
+						sender.Delete()
+						sender.Reply("本妞清除了不好的消息～", time.Duration(time.Second))
+						recalled = true
+						break
+					}
+				}
+			}
+		}
+		if recalled == true {
+			return
+		}
+	}
 }
 
 func FetchCookieValue(ps ...string) string {

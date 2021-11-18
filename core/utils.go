829e8d6d29e8a8d889a582337f5eb47d10c55cca
GitHub Action <action@github.com>
2021-11-18 14:25:04 +0000

    update

--- core/utils.go
+++ core/utils.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/google/uuid"
 )
@@ -22,4 +23,20 @@ func Float64(str interface{}) float64 {
 
 func RegistIm(i interface{}) Bucket {
 	return NewBucket(regexp.MustCompile("[^/]+$").FindString(reflect.TypeOf(i).PkgPath()))
+}
+
+func TrimHiddenCharacter(originStr string) string {
+	srcRunes := []rune(originStr)
+	dstRunes := make([]rune, 0, len(srcRunes))
+	for _, c := range srcRunes {
+		if c >= 0 && c <= 31 && c != 10 {
+			continue
+		}
+		if c == 127 {
+			continue
+		}
+
+		dstRunes = append(dstRunes, c)
+	}
+	return strings.ReplaceAll(string(dstRunes), "￼", "")
 }

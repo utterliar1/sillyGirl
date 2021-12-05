984d5b69a628909c2b4998c18ec1ba36c0e38690
GitHub Action <action@github.com>
2021-12-05 08:31:32 +0000

    update

--- im/wxmp/init.go
+++ im/wxmp/init.go
@@ -2,19 +2,14 @@ package wxgzh
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"reflect"
 	"strings"
 	"time"
 
-	"github.com/beego/beego/v2/adapter/httplib"
 	"github.com/cdle/sillyGirl/core"
 	"github.com/gin-gonic/gin"
-	wechat "github.com/silenceper/wechat/v2"
-	"github.com/silenceper/wechat/v2/cache"
-	offConfig "github.com/silenceper/wechat/v2/officialaccount/config"
-	"github.com/silenceper/wechat/v2/officialaccount/message"
+	"github.com/rixingyike/wechat"
 )
 
 var wxmp = core.NewBucket("wxmp")
@@ -23,87 +18,73 @@ var material = core.NewBucket("wxmpMaterial")
 func init() {
 	file_dir := "logs/wxmp/"
 	os.MkdirAll(file_dir, os.ModePerm)
+	wechat.Debug = true
+	cfg := &wechat.WxConfig{
+		AppId:          wxmp.Get("app_id"),
+		Secret:         wxmp.Get("app_secret"),
+		Token:          wxmp.Get("token"),
+		EncodingAESKey: wxmp.Get("encoding_aes_key"),
+		DateFormat:     "XML",
+	}
+	app := wechat.New(cfg)
 	core.Server.Any("/wx/", func(c *gin.Context) {
-		wc := wechat.NewWechat()
-		memory := cache.NewMemory()
-		cfg := &offConfig.Config{
-			AppID:          wxmp.Get("app_id"),
-			AppSecret:      wxmp.Get("app_secret"),
-			Token:          wxmp.Get("token"),
-			EncodingAESKey: wxmp.Get("encoding_aes_key"),
-			Cache:          memory,
-		}
-		officialAccount := wc.GetOfficialAccount(cfg)
+		ctx := app.VerifyURL(c.Writer, c.Request)
+		// switch ctx.Msg.MsgType {
+		// case wechat.TypeText:
+		// 	ctx.NewText(ctx.Msg.Content).Reply() // 回复文字
+		// case wechat.TypeImage:
+		// 	ctx.NewImage(ctx.Msg.MediaId).Reply() // 回复图片
+		// case wechat.TypeVoice:
+		// 	ctx.NewVoice(ctx.Msg.MediaId).Reply() // 回复语音
+		// case wechat.TypeVideo:
+		// 	ctx.NewVideo(ctx.Msg.MediaId, "video title", "video description").Reply() //回复视频
+		// case wechat.TypeFile:
+		// 	ctx.NewFile(ctx.Msg.MediaId).Reply() // 回复文件，仅企业微信可用
+		// default:
+		// 	ctx.NewText("其他消息类型" + ctx.Msg.MsgType).Reply() // 回复模板消息
+		// }
 
-		server := officialAccount.GetServer(c.Request, c.Writer)
-		server.SetMessageHandler(func(msg *message.MixMessage) *message.Reply {
-			if msg.Event == "subscribe" {
-				return &message.Reply{MsgType: message.MsgTypeText, MsgData: message.NewText(wxmp.Get("subscribe_reply", "感谢关注！"))}
-			}
-			sender := &Sender{}
-			sender.Message = msg.Content
-			sender.Wait = make(chan []interface{}, 1)
-			sender.uid = fmt.Sprint(msg.FromUserName)
-			core.Senders <- sender
-			end := <-sender.Wait
-			ss := []string{}
-			url := ""
-			if len(end) == 0 {
-				ss = append(ss, wxmp.Get("default_reply", "无法回复该消息"))
-			}
-			for _, item := range end {
-				switch item.(type) {
-				case error:
-					ss = append(ss, item.(error).Error())
-				case string:
-					ss = append(ss, item.(string))
-				case []byte:
-					ss = append(ss, string(item.([]byte)))
-				case core.ImageUrl:
-					url = string(item.(core.ImageUrl))
-				}
-			}
-			mediaID := ""
-			if url != "" && len(ss) == 0 {
-				filename := file_dir + fmt.Sprint(time.Now().UnixNano()) + ".jpg"
-				err := func() error {
-					f, err := os.Create(filename)
-					if err != nil {
-						return err
-					}
-					rsp, err := httplib.Get(url).Response()
-					_, err = io.Copy(f, rsp.Body)
-					if err != nil {
-						f.Close()
-						return err
-					}
-					f.Close()
-					m := officialAccount.GetMaterial()
-					mediaID, _, err = m.AddMaterial(message.MsgTypeImage, filename)
-					if err != nil {
-						return err
-					}
-					material.Set(mediaID, filename)
-					return nil
-				}()
-				if err != nil {
-					ss = append(ss, err.Error())
-					goto TEXT
-				}
-				return &message.Reply{MsgType: message.MsgTypeImage, MsgData: message.NewImage(mediaID)}
-			}
-		TEXT:
-			return &message.Reply{MsgType: message.MsgTypeText, MsgData: message.NewText(strings.Join(ss, "\n\n"))}
-		})
-		err := server.Serve()
-		if err != nil {
+		// data, _ := json.Marshal(ctx.Msg)
+		// fmt.Println(string(data))
+		// ctx.NewText(string(data)).Reply()
+		if ctx.Msg.Event == "subscribe" {
+			ctx.NewText(wxmp.Get("subscribe_reply", "感谢关注！")).Reply()
 			return
 		}
-		server.Send()
+		sender := &Sender{}
+		sender.Message = ctx.Msg.Content
+		sender.Wait = make(chan []interface{}, 1)
+		sender.uid = ctx.Msg.FromUserName
+		if wxmp.GetBool("isKe?", false) {
+			sender.ctx = ctx
+			core.Senders <- sender
+			return
+		}
+		core.Senders <- sender
+		end := <-sender.Wait
+		ss := []string{}
+		if len(end) == 0 {
+			ss = append(ss, wxmp.Get("default_reply", "无法回复该消息"))
+		}
+		for _, item := range end {
+			switch item.(type) {
+			case error:
+				ss = append(ss, item.(error).Error())
+			case string:
+				ss = append(ss, item.(string))
+			case []byte:
+				ss = append(ss, string(item.([]byte)))
+			case core.ImageUrl:
+				// url = string(item.(core.ImageUrl))
+			}
+		}
+		ctx.NewText(strings.Join(ss, "\n\n")).Reply()
 	})
+
 }
 
 type Sender struct {
+	ctx       *wechat.Context
 	Message   string
 	Responses []interface{}
 	Wait      chan []interface{}
@@ -160,6 +141,24 @@ func (sender *Sender) IsMedia() bool {
 }
 
 func (sender *Sender) Reply(msgs ...interface{}) (int, error) {
+
+	if sender.ctx != nil {
+		rt := ""
+		for _, item := range msgs {
+			switch item.(type) {
+			case error:
+				rt = item.(error).Error()
+			case string:
+				rt = item.(string)
+			case []byte:
+				rt = string(item.([]byte))
+			case core.ImageUrl:
+
+			}
+		}
+		sender.ctx.NewText(rt).Send()
+		return 0, nil
+	}
 	sender.Responses = append(sender.Responses, msgs...)
 	return 0, nil
 }
@@ -173,6 +172,9 @@ func (sender *Sender) Disappear(lifetime ...time.Duration) {
 }
 
 func (sender *Sender) Finish() {
+	if sender.ctx != nil {
+		return
+	}
 	if sender.Responses == nil {
 		sender.Responses = []interface{}{}
 	}

d15eb100fb7d745e1126431962b28f74a1bc59f7
GitHub Action <action@github.com>
2022-01-07 06:35:18 +0000

    update

--- im/wxmp/wv.go
+++ im/wxmp/wv.go
@@ -1,7 +1,6 @@
 package wxgzh
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/cdle/sillyGirl/core"
@@ -27,52 +26,25 @@ func init() {
 	core.Pushs["wxsv"] = func(i1 interface{}, s1 string, _ interface{}, _ string) {
 		app.SendText(fmt.Sprint(i1), s1)
 	}
+	core.OttoFuncs["GetWxmpAccessToken"] = app.GetAccessToken
 	core.Server.Any("/wxsv/", func(c *gin.Context) {
 		ctx := app.VerifyURL(c.Writer, c.Request)
+		sender := &Sender{}
+
 		if ctx.Msg.Event == "subscribe" {
-			ctx.NewText(wxsv.Get("subscribe_reply", "感谢关注！")).Reply()
-			return
+			sender.admin = true
+			ctx.Msg.Content = "wxsv subscribe"
 		}
 
 		if ctx.Msg.Event == "CLICK" {
+			sender.admin = true
 			ctx.Msg.Content = "wxsv " + ctx.Msg.EventKey
 		}
-		sender := &Sender{}
+
 		sender.tp = "wxsv"
 		sender.Message = ctx.Msg.Content
 		sender.uid = ctx.Msg.FromUserName
 		sender.ctx = ctx
 		core.Senders <- sender
-	})
-
-	core.AddCommand("", []core.Function{
-		{
-			Admin: true,
-			Rules: []string{"init wxsv"},
-			// Cron:  "1 1 * * *",
-			Handle: func(_ core.Sender) interface{} {
-				c := &core.Faker{
-					Type:    "carry",
-					Message: "wxsv init",
-					Carry:   make(chan string),
-				}
-				core.Senders <- c
-				f := ""
-				for {
-					v, ok := <-c.Listen()
-					if !ok {
-						break
-					}
-					f = v
-				}
-				bt := server.Menu{}
-				json.Unmarshal([]byte(f), &bt)
-				if len(bt.Button) < 0 {
-					return "没解析出菜单，" + f
-				}
-				app.AddMenu(&bt)
-				return f
-			},
-		},
 	})
 }
